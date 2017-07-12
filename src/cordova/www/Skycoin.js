8e25d6133a2778b7333eb8bea2d3f927bba751be
Unknown <xano3425@gmail.com>
2017-07-12 16:52:21 +0200

    Add www to Skycoin cordova plugin

---
+++ src/cordova/www/Skycoin.js
@@ -0,0 +1,26 @@
+var exec = require('cordova/exec');
+
+var Skycoin = {
+  createWallet: function(successCallback, errorCallback, args) {
+    console.log('creating wallet');
+    exec(successCallback, errorCallback, "Skycoin", "createwallet", args);
+  },
+  createAddress: function(successCallback, errorCallback, args) {
+    console.log('creating address');
+    exec(successCallback, errorCallback, "Skycoin", "createaddress", args);
+  },
+  getAddresses: function(successCallback, errorCallback, args) {
+    console.log('getting address in wallet');
+    exec(successCallback, errorCallback, "Skycoin", "getaddressinwallet", args);
+  },
+  getAddressBalance: function(successCallback, errorCallback, args) {
+    console.log('getting address balance');
+    exec(successCallback, errorCallback, "Skycoin", "getaddressinwallet", args);
+  },
+  sendSkycoin: function(successCallback, errorCallback, args) {
+    console.log('sending skycoin');
+    exec(successCallback, errorCallback, "Skycoin", "sendskycoin", args);
+  }
+};
+
+module.exports = Skycoin;
