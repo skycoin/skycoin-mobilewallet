57835b9444a01c5c6168fcafa897a1a38b4fa3a9
Unknown <xano3425@gmail.com>
2017-07-21 19:50:36 +0200

    Use new subject methods on wallet provider

--- src/pages/wallets/wallets.ts
+++ src/pages/wallets/wallets.ts
@@ -1,45 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { WalletProvider } from '../../providers/wallet/wallet.provider';
 import { WalletDetailPage } from '../wallet-detail/wallet-detail';
 import { WalletModel } from '../../models/wallet.model';
 import { SendSkycoinPage } from '../send-skycoin/send-skycoin';
 import { NewWalletPage } from '../new-wallet/new-wallet';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   templateUrl: 'wallets.html'
 })
-export class WalletsPage implements OnInit {
+export class WalletsPage {
 
   showCreateForm: boolean = false;
   sum: number = 0;
-  wallets: WalletModel[] = [];
+  wallets: Subject<WalletModel[]>;
 
   constructor(
-    private platform: Platform,
     public nav: NavController,
-    private wallet: WalletProvider,
+    public wallet: WalletProvider,
   ) {}
 
-  ngOnInit() {
-    this.platform.ready().then(() => this.getWallets());
-  }
-
   deleteWallet(wallet) {
-    this.wallet.destroy(wallet).subscribe(() => this.wallets = this.wallets.filter(o => o.id !== wallet.id));
-  }
-
-  getWallets() {
-    this.wallet.all().subscribe((wallets: WalletModel[]) => {
-      this.wallets = wallets;
-      wallets.forEach((wallet, index) => {
-        this.wallet.balance(wallet).subscribe(result => {
-          console.log(result);
-          this.sum = this.sum + result.balance;
-          this.wallets[index].balance = result.balance;
-        })
-      });
-    });
+    this.wallet.remove(wallet).subscribe();
   }
 
   openWallet(wallet) {
