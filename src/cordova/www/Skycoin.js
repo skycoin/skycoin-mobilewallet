4b9226c9526e4e99a43737a820e731d99db051ea
Unknown <xano3425@gmail.com>
2017-09-14 23:28:40 +0200

    Update Javascript interface for liteclient

--- src/cordova/www/Skycoin.js
+++ src/cordova/www/Skycoin.js
@@ -1,34 +1,18 @@
 var exec = require('cordova/exec');
 
 var Skycoin = {
-  createWallet: function(successCallback, errorCallback, args) {
-    console.log('creating wallet');
-    exec(successCallback, errorCallback, "Skycoin", "createwallet", args);
-  },
-  createAddress: function(successCallback, errorCallback, args) {
-    console.log('creating address');
-    exec(successCallback, errorCallback, "Skycoin", "createaddress", args);
-  },
   getAddresses: function(successCallback, errorCallback, args) {
+    console.log('creating wallet');
+    exec(successCallback, errorCallback, "Skycoin", "GetAddresses", args);
+  },
+  getBalances: function(successCallback, errorCallback, args) {
+    console.log('creating address');
+    exec(successCallback, errorCallback, "Skycoin", "GetBalances", args);
+  },
+  postTransaction: function(successCallback, errorCallback, args) {
     console.log('getting address in wallet');
-    exec(successCallback, errorCallback, "Skycoin", "getaddressinwallet", args);
+    exec(successCallback, errorCallback, "Skycoin", "PostTransaction", args);
   },
-  getAddressBalance: function(successCallback, errorCallback, args) {
-    console.log('getting address balance');
-    exec(successCallback, errorCallback, "Skycoin", "getbalance", args);
-  },
-  sendSkycoin: function(successCallback, errorCallback, args) {
-    console.log('sending skycoin');
-    exec(successCallback, errorCallback, "Skycoin", "sendskycoin", args);
-  },
-  getWalletbalance: function(successCallback, errorCallback, args) {
-    console.log('get wallet balance');
-    exec(successCallback, errorCallback, "Skycoin", "getblanceofwalletid", args);
-  },
-  generateSeed: function(successCallback, errorCallback, args) {
-    console.log('generate seed');
-    exec(successCallback, errorCallback, "Skycoin", "generateseed", args);
-  }
 };
 
 module.exports = Skycoin;
