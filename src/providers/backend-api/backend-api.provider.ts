1473440880096a3835c14c7f87945b87dc42444c
montycrypto <cryptomonty@gmail.com>
2017-11-11 16:56:52 +0100

    Add outputs call from backend

--- src/providers/backend-api/backend-api.provider.ts
+++ src/providers/backend-api/backend-api.provider.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Config } from '../../app/app.config';
 import { Observable } from 'rxjs/Observable';
+import { AddressModel } from '../../models/address.model';
+import { Output, OutputsResponse } from '../../app/app.datatypes';
 
 @Injectable()
 export class BackendApiProvider {
@@ -14,5 +16,15 @@ export class BackendApiProvider {
     const firstAddresses = addresses.length > 20 ? addresses.slice(0, 19) : addresses;
     const url = Config.backendUrl + 'transactions?addresses=' + firstAddresses.map(address => address.address).join(',');
     return this.http.get(url).map((res: any) => res.json());
+  }
+
+  getOutputs(addresses: AddressModel[]): Observable<Output[]> {
+    const firstAddresses = addresses.length > 20 ? addresses.slice(0, 19) : addresses;
+    const url = Config.backendUrl + 'outputs?addresses=' + firstAddresses.map(address => address.address).join(',');
+      return this.http.get(url).map((res: any) => {
+        const response: OutputsResponse = res.json();
+        response.head_outputs.forEach(output => output.coins = parseFloat(output.coins));
+        return response.head_outputs;
+      });
   }
 }
