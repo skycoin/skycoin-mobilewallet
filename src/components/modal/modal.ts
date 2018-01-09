7e55b367d2cdf8e06e5435993b0a3d3d693b20d2
devErnesto <dev0001@simelo.tech>
2018-01-09 09:05:05 -0500

    Add confirm modal wallet creation -Finsih

--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -1,9 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'modal',
-  templateUrl: 'modal.html'
+  selector: "modal",
+  templateUrl: "modal.html"
 })
 export class ModalComponent {
   @Input() header: string;
+  @Input() color: string;
+  @Output() close = new EventEmitter();
+
+  closeModal() {
+    this.close.emit();
+  }
 }
--- src/pages/add-wallet/add-wallet.ts
+++ src/pages/add-wallet/add-wallet.ts
@@ -1,44 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { WalletProvider } from '../../providers/wallet/wallet.provider';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { ViewController } from 'ionic-angular';
-import ModalComponent from './../../components/modal/modal';
+import { Component, OnInit } from "@angular/core";
+import { WalletProvider } from "../../providers/wallet/wallet.provider";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { ViewController } from "ionic-angular";
 
 @Component({
-  selector: 'page-add-wallet',
-  templateUrl: 'add-wallet.html',
+  selector: "page-add-wallet",
+  templateUrl: "add-wallet.html"
 })
 export class AddWalletPage implements OnInit {
-
   form: FormGroup;
   seed: string;
+  showDisclaimer: boolean = false;
+  disclaimerAccepted: boolean = false;
 
-  constructor(
-    private view: ViewController,
-    private wallet: WalletProvider,
-  ) {}
+  constructor(private view: ViewController, private wallet: WalletProvider) {}
 
   ngOnInit() {
     this.initForm();
   }
 
   cancel() {
-    this.view.dismiss()
+    this.view.dismiss();
   }
 
   createWallet() {
-    // this.wallet.create(this.form.value.label, this.form.value.seed);
-    // this.view.dismiss();
+    this.showDisclaimer = true;
+  }
+  hideModal() {
+    this.showDisclaimer = false;
+  }
+
+  confirm() {
+    this.wallet.create(this.form.value.label, this.form.value.seed);
+    this.view.dismiss();
   }
 
   generateSeed() {
-    this.wallet.generateSeed().subscribe(seed => this.form.controls.seed.setValue(seed));
+    this.wallet
+      .generateSeed()
+      .subscribe(seed => this.form.controls.seed.setValue(seed));
   }
 
   private initForm() {
     this.form = new FormGroup({
-      label: new FormControl('', Validators.required),
-      seed: new FormControl('', Validators.required),
+      label: new FormControl("", Validators.required),
+      seed: new FormControl("", Validators.required)
     });
 
     // this.generateSeed();
