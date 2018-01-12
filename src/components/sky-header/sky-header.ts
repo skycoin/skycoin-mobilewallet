d0f54821f8e127a6e118b82598a32395d5a3c010
devErnesto <dev0001@simelo.tech>
2018-01-12 12:14:37 -0500

    Update

--- src/components/sky-header/sky-header.ts
+++ src/components/sky-header/sky-header.ts
@@ -8,14 +8,14 @@ export class SkyHeaderComponent implements OnInit {
   @Input() title: string;
   @Input() sky: number;
   @Input() money: number;
-  @Input() sec_money: number;
+  @Input() secMoney: number;
   @Input() hours: number;
 
   ngOnInit() {
     this.title = this.title || 'Skycoin';
     this.sky = this.sky || 0;
     this.money = this.money || 0;
-    this.sec_money = this.sec_money || 0;
+    this.secMoney = this.secMoney || 0;
     this.hours = this.hours || 0;
   }
 }
