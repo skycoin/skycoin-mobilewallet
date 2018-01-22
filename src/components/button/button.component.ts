1cc7e1af4a12e1d7d46e6ca7cd4ec5b1bbf4ca81
devErnesto <dev0001@simelo.tech>
2018-01-22 09:06:46 -0500

    Update

--- src/components/button/button.component.ts
+++ src/components/button/button.component.ts
@@ -7,14 +7,29 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 export class ButtonComponent {
   @Input() disabled: any;
-  @Input() error: string;
-  @Input() state: number;
   @Output() action = new EventEmitter();
+
+  error: string;
+  state: number;
 
   onClick() {
     if (this.disabled) {
       return;
     }
     this.action.emit();
+  }
+
+  setLoading() {
+    this.state = 0;
+  }
+
+  setSuccess() {
+    this.state = 1;
+    setTimeout(() => this.state = null, 3000);
+  }
+
+  setError(error: any) {
+    this.error = error['_body'];
+    this.state = 2;
   }
 }
