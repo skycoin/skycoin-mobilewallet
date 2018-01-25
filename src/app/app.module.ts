66be0fe86a87008d054fdb4ba1bc71d775d30afb
devErnesto <dev0001@simelo.tech>
2018-01-25 10:07:11 -0500

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { ErrorHandler, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
-import { WalletsPage } from '../pages/wallets/wallets';
-import { MyApp } from './app.component';
-
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { Clipboard } from '@ionic-native/clipboard';
 import { File } from '@ionic-native/file';
 import { NativeStorage } from '@ionic-native/native-storage';
@@ -14,13 +9,13 @@ import { SecureStorage } from '@ionic-native/secure-storage';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule } from '@ionic/storage';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { BalanceComponent } from '../components/balance/balance';
 import { ButtonComponent } from '../components/button/button.component';
 import { HeaderComponent } from '../components/header/header';
 import { ModalComponent } from '../components/modal/modal';
 import { QrCodeComponent } from '../components/qr-code/qr-code';
 import { CounttoDirective } from '../directives/countto/countto';
-
 import { AddWalletPage } from '../pages/add-wallet/add-wallet';
 import { DisclaimerPage } from '../pages/disclaimer/disclaimer';
 import { LoadWalletPage } from '../pages/load-wallet/load-wallet';
@@ -30,7 +25,7 @@ import { SendSkycoinPage } from '../pages/send-skycoin/send-skycoin';
 import { TabsPage } from '../pages/tabs/tabs';
 import { TransactionsPage } from '../pages/transactions/transactions';
 import { WalletDetailPage } from '../pages/wallet-detail/wallet-detail';
-
+import { WalletsPage } from '../pages/wallets/wallets';
 import { AddressPipe } from '../pipes/address/address';
 import { SkyPipe } from '../pipes/sky/sky.pipe';
 import { WalletOptionPipe } from '../pipes/wallet-option/wallet-option';
@@ -40,6 +35,7 @@ import { PriceService } from '../providers/price/price.service';
 import { SecureStorageProvider } from '../providers/secure-storage/secure-storage';
 import { StorageApiProvider } from '../providers/storage-api/storage-api.provider';
 import { WalletProvider } from '../providers/wallet/wallet.provider';
+import { MyApp } from './app.component';
 
 @NgModule({
   declarations: [
