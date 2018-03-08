26a0c0d1fc66e6fc0e3b0428e0af92238db1645b
Ernesto Rodriguez <ernestomr87@gmail.com>
2018-03-08 10:52:31 -0500

    Add Close Button to Notification

--- src/components/notification/notification.ts
+++ src/components/notification/notification.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'notification',
@@ -7,4 +7,9 @@ import { Component, Input } from '@angular/core';
 export class NotificationComponent {
   @Input() text: string;
   @Input() color: string;
+  @Output() action = new EventEmitter();
+
+  onClick() {
+    this.action.emit();
+  }
 }
